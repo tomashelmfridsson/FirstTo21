462f27aa015822842ad15a0b14f477d8e2be8917
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-04-02 11:38:10 +0200

    Merge remote-tracking branch 'origin/branch'
    # Conflicts:
    #	src/main/java/games/firstto21/FirstTo21.java

--- src/main/java/games/firstto21/FirstTo21.java
+++ src/main/java/games/firstto21/FirstTo21.java
@@ -8,6 +8,7 @@ public class FirstTo21 {
         int currentNumber = 0;
         Random random= new Random();
         String red = "\u001B[31m";
+        String yellow = "\u001B[33m";
         System.out.println("🎲 Välkommen till 'Först till 26'! 🎲");
         System.out.println("Den som säger 26 vinner.");
         System.out.println("Du börjar! 🚀");
