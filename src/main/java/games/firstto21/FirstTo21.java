c84ac2e458fd04c222aa2bf407f9f8fd40201370
Felix Almqvist <kiwibirdi@hotmail.com>
2025-04-02 11:01:54 +0200

    -La till svårighetsgrader -La till random så inte datorn alltid vinner -La till skydd för att inte kunna skriva bokstäver

--- src/main/java/games/firstto21/FirstTo21.java
+++ src/main/java/games/firstto21/FirstTo21.java
@@ -1,19 +1,52 @@
 package games.firstto21;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class FirstTo21 {
+    enum DIFFICULTY{
+        EASY,
+        MEDIUM,
+        HARD,
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int currentNumber = 0;
+        DIFFICULTY currentDiff;
         String red = "\u001B[31m";
         System.out.println("Välkommen till spelet! Den som säger 21 vinner.");
+        System.out.println("Välj svårighetsgrad genom att skriva Easy, Medium eller Hard");
+        String s = scanner.nextLine();
+        switch(s.toLowerCase())
+        {
+            case "easy":
+                currentDiff = DIFFICULTY.EASY;
+                break;
+            case "medium":
+                currentDiff = DIFFICULTY.MEDIUM;
+                break;
+            case "hard":
+                currentDiff = DIFFICULTY.HARD;
+                break;
+            default:
+                currentDiff = DIFFICULTY.MEDIUM;
+                System.out.println("Ingen svårighetsgrad vald så vi går med Medium");
+                break;
+        }
+        System.out.println("Då kan spelet börja.");
         System.out.println("Du börjar!");
 
         while (true) {
             int playerMove = 0;
             while (playerMove != 1 && playerMove != 2) {
                 System.out.print("Säg 1 eller 2: ");
-                playerMove = scanner.nextInt();
+                try{
+                    playerMove = Integer.parseInt(scanner.nextLine());
+                } catch (Exception e) {
+                    System.out.println("Ogiltigt val, försök igen.");
+                    continue;
+                }
+
                 if (playerMove != 1 && playerMove != 2) {
                     System.out.println("Ogiltigt val, försök igen.");
                 }
@@ -27,7 +60,38 @@ public class FirstTo21 {
                 break;
             }
 
-            int computerMove = ((currentNumber+1) % 3) == 0 ? 1 : 2;
+            int computerMove;
+            int randomNum = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+            switch (currentDiff)
+            {
+                case DIFFICULTY.EASY:
+                    if(randomNum > 70)
+                    {
+                        computerMove = ((currentNumber+1) % 3) == 0 ? 1 : 2;
+                    }else {
+                        computerMove = ((currentNumber+1) % 3) == 0 ? 2 : 1;
+                    }
+                    break;
+                case DIFFICULTY.HARD:
+                    if(randomNum > 30)
+                    {
+                        computerMove = ((currentNumber+1) % 3) == 0 ? 1 : 2;
+                    }else {
+                        computerMove = ((currentNumber+1) % 3) == 0 ? 2 : 1;
+                    }
+                    break;
+                default:
+
+                    if(randomNum > 50)
+                    {
+                        computerMove = ((currentNumber+1) % 3) == 0 ? 1 : 2;
+                    }else {
+                        computerMove = ((currentNumber+1) % 3) == 0 ? 2 : 1;
+                    }
+                    break;
+            }
+
+
             currentNumber += computerMove;
             System.out.println("Datorn säger: " + currentNumber);
 
